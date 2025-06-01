7572e19536889d7d1faffdac8bf278ed84048532
Gyanaranjan Panda <gyana.ranjan@curebay.com>
2025-06-01 17:13:25 +0530

    new repo added

---
+++ src/main/java/com/curebay/interviewpreparation202525/InterviewPreparation202525Application.java
@@ -0,0 +1,13 @@
+package com.curebay.interviewpreparation202525;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPreparation202525Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewPreparation202525Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/curebay/interviewpreparation202525/InterviewPreparation202525ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curebay.interviewpreparation202525;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPreparation202525ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
